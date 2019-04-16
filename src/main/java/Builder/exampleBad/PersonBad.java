fe8055e9fbe2851a9b815b69bc32bcebed0040e1
max_yav <yavlanskiy@gmail.com>
2019-04-16 23:29:42 +0300

    Builder

---
+++ src/main/java/Builder/exampleBad/PersonBad.java
@@ -0,0 +1,14 @@
+package Builder.exampleBad;
+
+import Builder.example1.Person;
+
+import java.util.Set;
+
+public class PersonBad {
+    private String name;
+    private String surname;
+    private int age;
+    private int height;
+    private int weight;
+    private Set<Person> parents;
+}
